1e2413d274b56784e08c1822dbefda99cb6e5d5d
Simonas <Simonas@Simonas-PC>
2018-07-19 09:49:17 +0300

    11/23

--- junit5/test/com/in28minutes/junit5/StringTest.java
+++ junit5/test/com/in28minutes/junit5/StringTest.java
@@ -2,9 +2,36 @@ package com.in28minutes.junit5;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
 
 class StringTest {
+	
+	@BeforeAll
+	static void beforeAll() { //must be static
+		System.out.println("Connect to DB");
+	}
+	
+	@AfterAll
+	static void afterAll() { //must be static
+		System.out.println("Close Connection to DB");
+	}
+	
+	@BeforeEach //@Before ( in Junit4)
+	void beforeEachTest(TestInfo info) { // (only Junit5 feature (TestInfo info)
+		System.out.println("Print this before each test named " + info.getDisplayName());
+	}
+	
+	@AfterEach //@After ( in Junit4)
+	void AfterEachTest() {
+		System.out.println("Clean up Test Data after it");
+	}
+	
+	
 
 	@Test
 	void length_basic() {
@@ -28,7 +55,32 @@ class StringTest {
 		//assertEquals(true, result);
 		assertTrue(result);
 	}
+	
+	@Test //same as before, but inline
+	void contains_basic_inline() {
+		assertTrue("abcdefgh".contains("cd"));
+	}
 
+	@Test
+	void split_basic() {
+		String str = "abc def ghi";
+		String actualResult[] = str.split(" ");
+		String[] expectedResult = new String[] {"abc", "def", "ghi"};
+		
+		assertArrayEquals(expectedResult,actualResult);	
+	}
+	
+	@Test
+	void lenght_exception() {
+		String str = "NotNull";
+		assertThrows(NullPointerException.class,
+				() -> {
+					//code that throws exception
+					str.length();
+				}
+				
+				);
 
+	}
 }
  
